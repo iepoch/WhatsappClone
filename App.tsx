7491238a8a1abf1895d3c7b31c92742e43070710
iepoch <steaclem@gmail.com>
2021-07-29 18:46:00 -0700

    Updates - Added Graphql -  Implimented Backend of Users -  Created the Backend Schema for users

--- App.tsx
+++ App.tsx
@@ -10,11 +10,17 @@ import Navigation from "./navigation";
 import { withAuthenticator } from "aws-amplify-react-native";
 import Amplify from "aws-amplify";
 import config from "./src/aws-exports";
+import { useEffect } from "react";
 Amplify.configure(config);
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  // only run this when the App first mounted
+  useEffect(() => {
+    const fetchUser = async () => {};
+    fetchUser();
+  }, []);
   if (!isLoadingComplete) {
     return null;
   } else {
